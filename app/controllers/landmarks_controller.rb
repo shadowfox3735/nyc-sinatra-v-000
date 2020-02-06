dbb5151114735ce2a2bdc66148ea8001b1469e78
shadowfox3735 <normanpan180@gmail.com>
2020-02-06 01:16:30 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -30,5 +30,7 @@ class LandmarksController < ApplicationController
   patch '/landmarks/:id' do
     @landmark = Landmark.find(params[:id])
     @landmark.update(params[:landmark])
+    @landmark.save
+    redirect
   end
 end
