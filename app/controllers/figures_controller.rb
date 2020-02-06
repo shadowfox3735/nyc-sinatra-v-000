3c7e6732e9d8d8e57ae5acd2fe62d533a4b0005f
shadowfox3735 <normanpan180@gmail.com>
2020-02-06 00:38:20 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -37,6 +37,14 @@ class FiguresController < ApplicationController
   patch '/figures/:id' do
     @figure = Figure.find(params[:id])
     @figure.update(params[:figure])
+    unless params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
     
+    unless params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    
+    @figure.save
   end
 end
