f117991470c23f943384d1e0870075f5405353ec
shadowfox3735 <normanpan180@gmail.com>
2020-02-06 00:13:30 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,5 +1,9 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get '/figures' do
+    @figures = Figure.all
+  end
+  
   get '/figures/new' do
     erb :'/figures/new'
   end
