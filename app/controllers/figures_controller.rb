1fa92b6212e49fab46fbe376d04d0a9bb1efd51f
shadowfox3735 <normanpan180@gmail.com>
2020-02-06 00:11:33 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,26 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+  
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+
+    erb :'figures/show'
+  end
+  
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    unless params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+    
+    unless params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    
+    @figure.save
+    redirect_to "/figures/#{@figure.id}"
+  end
 end
